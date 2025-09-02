245bf065c1bfd514c32c70afa3c60b3c45405f07
Egor <leskovegor490@gmail.com>
2025-09-02 20:27:58 +0300

    settings gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/todo-list/',
   server: {
     host: true,
     port: 8080,
